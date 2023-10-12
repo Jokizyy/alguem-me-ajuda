915dca8bdac5d83a0f6fac775840551205c4aa71
Jokizyy <147684738+Jokizyy@users.noreply.github.com>
2023-10-12 01:38:34 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,91 @@
+export default [
+  {
+    question: "Quanto é 1 + 2?",
+    answers: [
+      { option: "3", correct: true },
+      { option: "2", correct: false },
+      { option: "4", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 5 x 2?",
+    answers: [
+      { option: "9", correct: false },
+      { option: "10", correct: true },
+      { option: "11", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 10 x 8?",
+    answers: [
+      { option: "800", correct: false },
+      { option: "90", correct: false },
+      { option: "80", correct: true },
+    ],
+  },
+  {
+    question: "Quanto é 7 x 7?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "Quanto é 15 ÷ 3?",
+    answers: [
+      { option: "3", correct: true },
+      { option: "5", correct: false },
+      { option: "6", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 4 + 7?",
+    answers: [
+      { option: "11", correct: true },
+      { option: "10", correct: false },
+      { option: "12", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 9 x 9?",
+    answers: [
+      { option: "81", correct: true },
+      { option: "72", correct: false },
+      { option: "90", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 18 - 5?",
+    answers: [
+      { option: "13", correct: true },
+      { option: "12", correct: false },
+      { option: "11", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 25 + 15?",
+    answers: [
+      { option: "40", correct: true },
+      { option: "35", correct: false },
+      { option: "45", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 2 x 6?",
+    answers: [
+      { option: "12", correct: true },
+      { option: "10", correct: false },
+      { option: "8", correct: false },
+    ],
+  },
+  {
+    question: "Quanto é 100 ÷ 5?",
+    answers: [
+      { option: "20", correct: true },
+      { option: "15", correct: false },
+      { option: "25", correct: false },
+    ],
+  },
+]; 
+
